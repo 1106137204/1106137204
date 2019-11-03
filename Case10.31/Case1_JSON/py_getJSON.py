3cfdb2d4d1310ce032748e4bae3d1adaece01be8
1106137204 <1106137204@nkust.edu.tw>
2019-11-03 22:04:35 +0800

    回家作業

--- Case10.31/Case1_JSON/py_getJSON.py
+++ Case10.31/Case1_JSON/py_getJSON.py
@@ -3,7 +3,5 @@
 
 import requests
 import json
-data = requests.get(url="http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3")
-with open("music.json","w",encoding="utf-8") as myFile:
-    json.dump(data.json(), myFile,ensure_ascii=False)
-myFile.close()
+data = requests.get(url="https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=4")
+print(data)
